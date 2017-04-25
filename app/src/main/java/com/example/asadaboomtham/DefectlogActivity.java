12711ccf1a6e20c32b4974f85c9ea5841fc00118
aod <se57160023aod@gmail.com>
2017-04-25 09:57:53 +0700

    aod

---
+++ app/src/main/java/com/example/asadaboomtham/DefectlogActivity.java
@@ -0,0 +1,15 @@
+package com.example.asadaboomtham;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asadaboomtham.softwaretestingmanagesysapplication.R;
+
+public class DefectlogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defectlog);
+    }
+}
